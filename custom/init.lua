4f8ece0f7bd71e3b08ae0233b365bdb2074df4f1
Patrick Stockton <patrick@codejnki.com>
2023-10-24 12:24:26 -0400

    init to win it

---
+++ custom/init.lua
@@ -0,0 +1,7 @@
+vim.wo.relativenumber = true
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
